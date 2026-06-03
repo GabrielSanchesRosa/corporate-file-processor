797d7afbb7fcd4172a6ce3321ff9386be88eae35
Gabriel Sanches <gabrielsanches131202@gmail.com>
2026-06-03 20:13:47 -0300

    first commit

---
+++ src/main/java/br/com/gsr/corporate_file_processor/CorporateFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package br.com.gsr.corporate_file_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateFileProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateFileProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gsr/corporate_file_processor/CorporateFileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gsr.corporate_file_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateFileProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
